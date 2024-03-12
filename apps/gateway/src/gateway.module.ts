620f2a39006878d0f31c85e4971ade5de70b8594
puntonemo <code@puntonemo.com>
2024-03-12 07:31:32 +0100

    wip: config port

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,18 +1,34 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './modules/products/products.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
     imports: [
-        ClientsModule.register([
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+        ClientsModule.registerAsync([
             {
                 name: 'SERVICE_PRODUCTS',
-                transport: Transport.TCP,
-                options: {
-                    host: 'localhost',
-                    port: 3001
-                }
+                useFactory: (configService: ConfigService) => ({
+                    transport: Transport.TCP,
+                    options: {
+                        host: configService.get('SERVICE_PRODUCTS_HOST'),
+                        port: configService.get('SERVICE_PRODUCTS_PORT')
+                    }
+                })
             }
         ])
+        // ClientsModule.register([
+        //     {
+        //         name: 'SERVICE_PRODUCTS',
+        //         transport: Transport.TCP,
+        //         options: {
+        //             host: 'localhost',
+        //             port: 3001
+        //         }
+        //     }
+        // ])
     ],
     controllers: [ProductsController],
     providers: []
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
     const logger = new Logger('GATEWAY');
     const app = await NestFactory.create(GatewayModule);
-    await app.listen(3000);
-    logger.log('Gateway is running on port 3000');
+    const configService = app.get<ConfigService>(ConfigService);
+    const PORT = configService.get('PORT') || 3000;
+    await app.listen(PORT);
+    logger.log(`Gateway is running on port ${PORT}`);
 }
 bootstrap();
